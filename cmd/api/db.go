3439a5d43f430f9c122750c67063fccf7436c726
Bobzitto <soujv91@hotmail.com>
2024-10-10 11:52:57 -0300

    deploy again

--- cmd/api/db.go
+++ cmd/api/db.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+// openDB initializes a connection to the database using the provided DSN.
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
@@ -23,12 +26,24 @@ func openDB(dsn string) (*sql.DB, error) {
 	return db, nil
 }
 
+// connectToDB establishes a connection to the database using environment variables.
 func (app *application) connectToDB() (*sql.DB, error) {
-	connection, err := openDB(app.DSN)
+	// Retrieve database connection details from environment variables
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	// Construct the Data Source Name (DSN)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+		host, port, user, password, dbname)
+
+	connection, err := openDB(dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	log.Println("Connected!")
+	log.Println("Connected to the database!")
 	return connection, nil
 }
